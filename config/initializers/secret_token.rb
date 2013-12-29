05603547088fb95c1bc28e766bcfacf2b36553c8
Cory Gwin <gwincr11@yahoo.com>
2013-12-28 19:53:32 -0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Analogforest::Application.config.secret_key_base = 'f0bceaaa4c1a90f6ac74055953492ecf97d7e2d22551af646f8251c53501398c26fc83be5c8132e4f0bdde17f155abb94bd141abff46dfdafa2a1ccfba91d370'
